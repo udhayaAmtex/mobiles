63bf534309ddec0c7e4551024852fef01b61967a
LAPTOP512\UdhayaKumar <udhya.kumar@amtexsystems.com>
2024-01-04 18:51:40 +0530

    website of a company

---
+++ product.js
@@ -0,0 +1,10 @@
+let productList = [
+    {name : 'REDMI NOTE 13' , desc : "Brief description of Product 1.", img : 'images.jpg' , type : '4G'},
+    {name : 'VIVO Y16' , desc : "Brief description of Product 2.", img : 'images.jpg' , type : '4G' },
+    {name : 'REDMI NOTE 1' , desc : "Brief description of Product 2.", img : 'images.jpg' , type : '5G'},
+    {name : 'REDMI NOTE 14' , desc : "Brief description of Product 2.", img : 'images.jpg' , type : '4G'},
+    {name : 'REDMI NOTE 12' , desc : "Brief description of Product 2.", img : 'images.jpg' , type : '4G'},
+    {name : 'REDMI NOTE 2' , desc : "Brief description of Product 2.", img : 'images.jpg' , type : '5G'},
+    {name : 'REDMI NOTE 3' , desc : "Brief description of Product 2.", img : 'images.jpg' , type : '5G'},
+    {name : 'REDMI NOTE 4' , desc : "Brief description of Product 2.", img : 'images.jpg' , type : '5G'}
+];
