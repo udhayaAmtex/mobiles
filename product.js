a5eebccd78a378f19f61177ab272d599d82c79bd
LAPTOP512\UdhayaKumar <udhya.kumar@amtexsystems.com>
2024-01-05 17:55:04 +0530

    updated view product

--- product.js
+++ product.js
@@ -5,8 +5,8 @@ var productList = [
     {id : 4 , name : 'REDMI NOTE 14' , desc : "Brief description of Product 2.", img : 'images/images3.jpg' , type : '4G'},
     {id : 5 , name : 'REDMI NOTE 12' , desc : "Brief description of Product 2.", img : 'images/images.jpg' , type : '4G'},
     {id : 6 , name : 'REDMI NOTE 2' , desc : "Brief description of Product 2.", img : 'images/images1.jpg' , type : '5G'},
-    {id : 6 , name : 'REDMI NOTE 3' , desc : "Brief description of Product 2.", img : 'images/images2.jpg' , type : '5G'},
-    {id : 7 , name : 'REDMI NOTE 4' , desc : "Brief description of Product 2.", img : 'images/images3.jpg' , type : '5G'}
+    {id : 7 , name : 'REDMI NOTE 3' , desc : "Brief description of Product 2.", img : 'images/images2.jpg' , type : '5G'},
+    {id : 8 , name : 'REDMI NOTE 4' , desc : "Brief description of Product 2.", img : 'images/images3.jpg' , type : '5G'}
 ];
 
 var selectedProduct = {id : 1 , name : 'Dummy phone' , desc : "Good Product", img : 'images.jpg' , type : '4G'}
